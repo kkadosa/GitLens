39a4474a88d9627d84a19ab49792b33427485830
Kadosa Koltai <kadosa.koltai@incquerylabs.com>
2019-09-09 17:58:20 +0200

    Fixed project setup.

---
+++ src/main/java/hu/bme/mit/gitlens/GitLens.java
@@ -0,0 +1,17 @@
+package hu.bme.mit.gitlens;
+
+import hu.bme.mit.curiosity.Plugin;
+import javafx.stage.Stage;
+
+class GitLens implements Plugin {
+
+    @Override
+    public void load(Stage stage) {
+
+    }
+
+    @Override
+    public void unload() {
+
+    }
+}
