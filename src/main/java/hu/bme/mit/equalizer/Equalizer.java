d58e427ba99c9cbf20bab7db7e0c43ba80adde93
Kadosa Koltai <kadosa.koltai@incquerylabs.com>
2019-09-17 12:40:47 +0200

    Decided on Vert.x as Rest server.

--- src/main/java/hu/bme/mit/equalizer/Equalizer.java
+++ src/main/java/hu/bme/mit/equalizer/Equalizer.java
@@ -1,12 +1,20 @@
 package hu.bme.mit.equalizer;
 
 import hu.bme.mit.platform.Plugin;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
-public class Equalizer implements Plugin {
+public class Equalizer extends AbstractVerticle implements Plugin {
+
+    public static Router masterRouter;
 
     @Override
     public void load() {
         //TODO database button, client button, "update eagerly", "mostly one way", "level"
+        Vertx.vertx().deployVerticle(this);
     }
 
     @Override
@@ -17,5 +25,22 @@ public class Equalizer implements Plugin {
     @Override
     public boolean isGui() {
         return true;
+    }
+
+    @Override
+    public void start() {
+        Vertx vertx = Vertx.vertx();
+        Router masterRouter = Router.router(vertx);
+        masterRouter.get().handler(this::get);
+        masterRouter.put().handler(this::put);
+        HttpServer server = vertx.createHttpServer();
+        server.requestHandler(masterRouter).listen();
+    }
+
+    private void get(RoutingContext routingContext) {
+        
+    }
+
+    private void put(RoutingContext routingContext) {
     }
 }
--- src/main/java/hu/bme/mit/equalizer/RepositoryLens.java
+++ src/main/java/hu/bme/mit/equalizer/RepositoryLens.java
@@ -1,11 +1,11 @@
 package hu.bme.mit.equalizer;
 
-import org.json.JSONObject;
+import io.vertx.core.json.JsonObject;
 
 import java.net.URI;
 import java.util.concurrent.CompletableFuture;
 
 public interface RepositoryLens {
-    void get(URI uri, JSONObject payload, CompletableFuture<String> out);
-    void put(URI uri, JSONObject payload, CompletableFuture<String> out);
+    void get(URI uri, JsonObject payload, CompletableFuture<String> out);
+    void put(URI uri, JsonObject payload, CompletableFuture<String> out);
 }
